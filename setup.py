7387eba252de5e74850d4c3d53f4e71949ce8c63
roschly <roschly@gmail.com>
2021-10-28 20:26:56 +0200

    add download link to setup.py + new version

--- setup.py
+++ setup.py
@@ -3,18 +3,18 @@ from setuptools import setup, find_packages
 
 
 setup(
-    name='outlinepy',
-    description='Commandline tool for outlining Python code.',
-    url='https://github.com/roschly/outlinepy',
-    # download_url='https://github.com/roschly/moduml/archive/refs/tags/v0.1.1.tar.gz',
-    author='roschly',
+    name="outlinepy",
+    description="Commandline tool for outlining Python code.",
+    url="https://github.com/roschly/outlinepy",
+    download_url="https://github.com/roschly/outlinepy/archive/refs/tags/v0.0.2.tar.gz",
+    author="roschly",
     entry_points={
-        'console_scripts': [
-            'outlinepy = outlinepy.__main__:main',
+        "console_scripts": [
+            "outlinepy = outlinepy.__main__:main",
         ]
     },
     packages=find_packages(),
     install_requires=[
-        'astroid',
-    ]
+        "astroid",
+    ],
 )
